6a41a7fc363a1f8bec990d80fd2503f779c5bced
Hari Krishnan <hari@qburst.com>
2016-02-12 10:56:17 +0530

    first commit

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,5 @@ var spaceApp = angular.module('spaceApp', ['ngRoute']);
 
 spaceApp.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-      when('/hrk', {
-        templateUrl: 'partials/vacation.html',
-        controller: ''
-      })
+   	
   }]);
