e611c7b75a7d02064939d6cb072e96afc8a31178
Hari Krishnan <hari@qburst.com>
2016-02-16 17:48:47 +0530

    vacation second commit

--- app/controllers/vacationController.js
+++ app/controllers/vacationController.js
@@ -1,5 +1,26 @@
-spaceApp.controller('vacationController',['$scope', function($scope){
+spaceApp.controller('vacationController',['$scope','User','$http', function($scope,User,$http){
+	$scope.user = User.query()
+	$scope.noDays=0;
+	var vacationType =["Loss of Pay","Compensatory","Maternity","Paternity"];
+	$http.get('../assets/JSON/approver.json').success(function(data) {
+    	$scope.approvers = data;
+    });
+
+	$http.get('../assets/JSON/vacation.json').success(function(data) {
+    	$scope.vacations = data;
+    });
 	$scope.submit = function() {
-		console.log("clicked submit")
+		// if($scope.from&&$scope.to&&$scope.noDays&&$scope.sendTo){
+			var from = $scope.from.getMonth()+"/"+$scope.from.getDate()+"/"+$scope.from.getFullYear();
+			var to = $scope.to.getMonth()+"/"+$scope.to.getDate()+"/"+$scope.to.getFullYear();
+			$scope.vacations.push({
+								"dateRange" : from +"-"+ to,
+								"noDays" : $scope.noDays,
+								"vacationType" : vacationType[$scope.vacationType-1],
+								"approver" : $scope.sendTo,
+								"status" : "pending",
+								"remarks" : ""
+							});
+		// }
 	};
 }]);
--- app/services/currentUserService.js
+++ app/services/currentUserService.js
@@ -2,6 +2,6 @@ var currentUserServices = angular.module('currentUserServices', ['ngResource']);
 
 currentUserServices.factory('User', ['$resource',function($resource){
     return $resource('../assets/JSON/currentUser.json', {}, {
-      query: {method:'GET',isArray:true}
+      query : {method:'GET',isArray:true}
     });
   }]);
