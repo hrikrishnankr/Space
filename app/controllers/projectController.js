7c54af4f40394da001fb6cf60832e48295ab9676
geethujp <geethuj@qburst.com>
2016-02-17 17:50:18 +0530

    Included ng-table, JSON file fetch ang ng-show

---
+++ app/controllers/projectController.js
@@ -0,0 +1,37 @@
+spaceApp.controller("projectController", function($scope, $http) {
+   
+
+         $http.get("../assets/JSON/project.json")
+            .success(function (data) {$scope.details =data;
+               $scope.status_options=$scope.details.stats;
+              $scope.vertical_options=$scope.details.verticals;
+                $scope.bill_type_options=$scope.details.billtypes;
+                $scope.category_options=$scope.details.categories;
+                console.log($scope.status_options);
+                 
+            });
+   localStorage.clear();
+    $scope.saved = localStorage.getItem('projects');
+    $scope.projects = (localStorage.getItem('projects') !== null) ? JSON.parse($scope.saved) : [];
+  $scope.showHide=function()
+{
+    $scope.display=!$scope.display;
+    console.log($scope.display);
+}
+     
+    $scope.submit = function() {
+        $scope.projects.push({
+            project: $scope.project,
+            customer: $scope.customer,
+            pm: $scope.pm,
+            billtype: $scope.billtype,
+            sdate: $scope.sdate,
+            tech: $scope.tech,
+            stat: $scope.stat,
+            vertical: $scope.vertical,
+            category: $scope.category,
+            detail: $scope.detail
+        });
+        localStorage.setItem('projects', JSON.stringify($scope.projects));
+    }
+});
