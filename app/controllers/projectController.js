8463497562e82692c4ae94cd4bb69c6ecfe6aecc
geethujp <geethuj@qburst.com>
2016-02-25 17:18:53 +0530

    Removed consoles

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -1,6 +1,5 @@
 spaceApp.controller('projectController', ['$routeParams', '$scope', 'localstorageService' ,function($routeParams,$scope,localstorageService) {
    var id = $routeParams.id;
- console.log(id);
  var local=  localstorageService.getObject('projects');
    $scope.details =_.find(local, function(item) {
     return item.id ==$routeParams.id; 
